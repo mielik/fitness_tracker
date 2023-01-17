3b9028a19ccfce3beda785fc9940a03742eb12dd
Derbasova <anna1.derbasova@gmail.com>
2023-01-17 09:52:04 +0100

    Format docstirngs

--- homework.py
+++ homework.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 
 @dataclass
 class InfoMessage:
-    """Информационное сообщение о тренировке."""
+    """Training information message.."""
     training_type: str
     duration: float
     distance: float
@@ -12,15 +12,15 @@ class InfoMessage:
     calories: float
 
     def get_message(self) -> str:
-        return (f"Тип тренировки: {self.training_type}; "
-                f"Длительность: {self.duration:.3f} ч.; "
-                f"Дистанция: {self.distance:.3f} км; "
-                f"Ср. скорость: {self.speed:.3f} км/ч; "
-                f"Потрачено ккал: {self.calories:.3f}.")
+        return (f"Type of training: {self.training_type}; "
+                f"Duration: {self.duration:.3f} ч.; "
+                f"Distance: {self.distance:.3f} км; "
+                f"Average speed: {self.speed:.3f} км/ч; "
+                f"Burned calories: {self.calories:.3f}.")
 
 
 class Training:
-    """Базовый класс тренировки."""
+    """Basic training class."""
     LEN_STEP: float = 0.65
     M_IN_KM: int = 1000
     MIN_IN_H: int = 60
@@ -35,26 +35,26 @@ class Training:
         self.weight = weight
 
     def get_distance(self) -> float:
-        """Получить дистанцию в км."""
+        """Get the distance in km."""
         return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
+        """Get the average speed."""
         return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
+        """Count calories."""
         raise NotImplementedError("Subclasses should implement this.")
 
     def show_training_info(self) -> InfoMessage:
-        """Вернуть информационное сообщение о выполненной тренировке."""
+        """Return an informational message about the completed workout."""
         return InfoMessage(type(self).__name__, self.duration,
                            self.get_distance(), self.get_mean_speed(),
                            self.get_spent_calories())
 
 
 class Running(Training):
-    """Тренировка: бег."""
+    """Training: Running."""
     CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
     CALORIES_MEAN_SPEED_SHIFT: int = 1.79
 
@@ -67,7 +67,7 @@ class Running(Training):
 
 
 class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
+    """Training: SportsWalking."""
     CALORIES_WEIGHT_MULTIPLIER: float = 0.035
     CALORIES_SPEED_HEIGHT_MULTIPLIER: float = 0.029
     KMH_IN_MSEC: float = 0.278
@@ -91,7 +91,7 @@ class SportsWalking(Training):
 
 
 class Swimming(Training):
-    """Тренировка: плавание."""
+    """Training: Swimming."""
     LEN_STEP: float = 1.38
     SPEED_MEAN_MULTIPLER: float = 1.1
     WEIGHT_MULTIPLER: int = 2
@@ -107,7 +107,7 @@ class Swimming(Training):
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
+        """Get the average speed."""
         return (self.length_pool * self.count_pool
                 / self.M_IN_KM / self.duration)
 
@@ -117,7 +117,7 @@ class Swimming(Training):
 
 
 def read_package(workout_type: str, data: List[int]) -> Training:
-    """Прочитать данные полученные от датчиков."""
+    """Read the data received from the sensors."""
     workout_types: Dict[str, Training] = {
         "SWM": Swimming,
         "RUN": Running,
@@ -129,7 +129,7 @@ def read_package(workout_type: str, data: List[int]) -> Training:
 
 
 def main(training: Training) -> InfoMessage:
-    """Главная функция."""
+    """Main function."""
     info: InfoMessage = training.show_training_info()
     print(info.get_message())
 
